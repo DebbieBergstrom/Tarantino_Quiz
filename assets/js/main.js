22bb084da41b20304ff8869f6fa1f542f3aa48ec
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-06-09 19:53:04 +0000

    Add const variables in JS for sites main containers

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const homeBox = document.querySelector('#homebox'); 
+const rulesBox = document.querySelector('.rules-box'); 
+const nameFirstBox = document.querySelector('.name-first-box');
+const gameBox = document.querySelector('.game-box');
+const finishedBox = document.querySelector('.finished-box');
+const scoreBox = document.querySelector('.score-box');
