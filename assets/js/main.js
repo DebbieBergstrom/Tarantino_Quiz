29e1a1c5f4dd431cbcfc6ef2172bb13569b8a827
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-06-09 17:29:26 +0000

    Add Enter name first box and aria labels to buttons

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const rules = document.querySelector('.rules-box');
+const startQuiz = document.querySelector('.btn-game');
+
+
+const openRules = document.querySelector('#btn-rules');
+openRules.addEventListener("click", () => rules.classList.remove("hidden"));
+
+
+const homePage = document.querySelector('#home-box');
