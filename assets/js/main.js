b25dcda9a10f645d7549daa6de41c3ea4cc5f002
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-06-10 09:05:22 +0000

    Add score pages variables to js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
+// Get boxes 
 const homeBox = document.querySelector('#homebox'); 
-const rulesBox = document.querySelector('.rules-box'); 
-const nameFirstBox = document.querySelector('.name-first-box');
-const gameBox = document.querySelector('.game-box');
-const finishedBox = document.querySelector('.finished-box');
-const scoreBox = document.querySelector('.score-box');
+const rulesBox = document.querySelector('#rules-box'); 
+const nameFirstBox = document.querySelector('#name-first-box');
+const gameBox = document.querySelector('#game-box');
+const finishedBox = document.querySelector('#finished-box');
+const scoreBox = document.querySelector('#score-box');
+
+// Get buttons
+const btnRules = document.querySelector('#btn-rules');
+const btn-Score = document.querySelector('.btn-highscore');
+const btnNameFirst = document.querySelector('.btn-to-name-first');
+const scoreExit = document.querySelector('.btn-exit');
+
+let finishedImage = document.querySelector('.finished-quiz-img-bg');
+let scoreList = document.querySelector('#score-list');
+
